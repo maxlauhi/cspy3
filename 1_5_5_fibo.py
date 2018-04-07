d2e5295295f79e294c37ed5932882b1db6abed15
maxlauhi <maxlauhi@gmail.com>
2018-04-07 21:32:24 +0800

    complete fibonacci numbers calculate

--- 1_5_5_fibo.py
+++ 1_5_5_fibo.py
@@ -1,8 +1,12 @@
-"""compute Nth Fibonacci number.
-    >>> fibo(5)
-    5
+"""compute Nth Fibonacci number,for n >= 2.
+    >>> fibo(8)
+    13
 """
 
 def fibo(n):
-    assert n<0, "N must be a positive number"
-    
+    pred, curr = 0, 1 # Fibonacci number 1th and 2th
+    k = 2             # No more less
+    while k < n:
+        pred, curr = curr, pred + curr
+        k = k + 1
+    return curr
