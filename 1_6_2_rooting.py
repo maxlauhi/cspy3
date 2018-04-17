2ca6a325aba1dd6d24a49d92d63c6c1fcf940a03
maxlauhi <maxlauhi@gmail.com>
2018-04-17 17:58:16 +0800

    edit functions of square and cube root

--- 1_6_2_rooting.py
+++ 1_6_2_rooting.py
@@ -1,10 +1,25 @@
-def square_root(a):
-    x = 1
-    while x * x != a:
-        print(x)
-        print(a)
-        x = square_root_update(x, a)
-    return x
-
 def square_root_update(x, a):
-    return ((x + a/x))/2
+    return (x + a/x) / 2
+
+def cube_root_update(x, a):
+    return (2*x + a/(x*x))/3
+
+def improve(update, close, guess=1):
+    while not close(guess):
+        guess = update(guess)
+    return guess
+
+def approx_eq(x, y, tolerance=1e-15):
+    return abs(x-y) < tolerance
+
+def square_root(a):
+    def update(x):
+        return square_root_update(x, a)
+    def close(x):
+        return approx_eq(x*x, a)
+    return improve(update, close)
+
+def cube_root(a):
+    return improve(lambda x: cube_root_update(x, a),
+                   lambda x: approx_eq(x*x*x, a))
+  
