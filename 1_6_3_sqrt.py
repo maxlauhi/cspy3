3201c8a7a33ea578d80294f4e2fc7ae49b3923ad
maxlauhi <maxlauhi@gmail.com>
2018-04-20 07:48:15 +0800

    edit tolerance value for sqrt

--- 1_6_3_sqrt.py
+++ 1_6_3_sqrt.py
@@ -6,13 +6,15 @@ def improve(update, close, guess=1):
         guess = update(guess)
     return guess
 
-def approx_eq(x, y, tolerance=1e4):
+def approx_eq(x, y, tolerance=1e-3):
     return abs(x - y) < tolerance
 
 def sqrt(a):
+    print(a)
     def sqrt_update(x):
         return average(x, a/x)
     def sqrt_close(x):
+        print(x)
         # true or flase from func(approx_eq)
         return approx_eq(x * x, a)
     return improve(sqrt_update, sqrt_close)
