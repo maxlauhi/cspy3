a15c1dd507c5966af3cec1dd78fb410cfca88caa
maxlauhi <maxlauhi@gmail.com>
2018-04-27 23:25:28 +0800

    Mutual recursion for even or odd

---
+++ 1_7_2_mu_rec.py
@@ -0,0 +1,12 @@
+"""determine whether a number is odd or even by mutual recursion"""
+def is_even(n):
+    if n == 0:
+        return True
+    else:
+        return is_odd(n-1)
+
+def is_odd(n):
+    if n == 0:
+        return False
+    else:
+        return is_even(n-1)
