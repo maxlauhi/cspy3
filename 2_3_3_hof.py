a358c1af49c9556daa348a9ae37682b542b68059
maxlauhi <maxlauhi@gmail.com>
2018-05-16 11:37:15 +0800

    finished and updated sequence processing

--- 2_3_3_hof.py
+++ 2_3_3_hof.py
@@ -1,5 +1,5 @@
 """Higher-Order Functions with Sequence"""
-from operator import mul
+from operator import mul, add
 
 def apply_to_all(map_fn, s):
     return [map_fn(x) for x in s]
@@ -13,3 +13,23 @@ def reduce(reduce_fn, s, initial):
         reduced = reduce_fn(reduced, x)
     return reduced
 
+def divisors_of(n):
+    divides_n = lambda x: n % x == 0
+    return [1] + keep_if(divides_n, range(2, n))
+
+def sum_of_divisors(n):
+    return reduce(add, divisors_of(n), 0)
+
+def perfect(n):
+    return sum_of_divisors(n) == n
+
+"""
+>>> keep_if(perfect, range(0, 1000))
+>>> [1, 6, 28, 496]
+
+Conventional Names:
+    common names do not return lists.
+    the difinitions above are equivalent to:
+        apply_to_all = lambda map_fn, s: list(map(map_fn, s))
+        keep_if = lambda filter_fn, s: list(filter_fn, s))
+"""
