a46622d82572d7b531345e1b2cf878ad097d528f
maxlauhi <maxlauhi@gmail.com>
2018-05-18 18:01:34 +0800

    add example of trees

---
+++ 2_3_6_trees.py
@@ -0,0 +1,10 @@
+def tree():
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+        return [root_label] + list(branches)
+
+def label():
+
+def branches():
+
+def is_tree():
