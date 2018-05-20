8f8ad5a66c037b79a7b4f8d6e2c603b100a12649
maxlauhi <maxlauhi@gmail.com>
2018-05-20 15:57:15 +0800

    add Fibonacci number on trees

--- 2_3_6_trees.py
+++ 2_3_6_trees.py
@@ -1,10 +1,30 @@
-def tree():
+def tree(root_label, branches=[]):
     for branch in branches:
         assert is_tree(branch), 'branches must be trees'
-        return [root_label] + list(branches)
+    return [root_label] + list(branches)
 
-def label():
+def label(tree):
+    return tree[0]
 
-def branches():
+def branches(tree):
+    return tree[1:]
 
-def is_tree():
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    return not branches(tree)
+
+def fib_tree(n):
+    if n == 0 or n == 1:
+        return tree(n)
+    else:
+        left, right = fib_tree(n-2), fib_tree(n-1)
+        fib_n = label(left) + label(right)
+        return tree(fib_n, [left, right])
+    
