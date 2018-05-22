dd15dbdb3784bbec7d91b8770eb9d38ed538970a
maxlauhi <maxlauhi@gmail.com>
2018-05-22 18:03:23 +0800

    updating trees to Partition trees

--- 2_3_6_trees.py
+++ 2_3_6_trees.py
@@ -27,4 +27,29 @@ def fib_tree(n):
         left, right = fib_tree(n-2), fib_tree(n-1)
         fib_n = label(left) + label(right)
         return tree(fib_n, [left, right])
-    
+def count_leaves(tree):
+    if is_leaf(tree):
+        return 1
+    else:
+        branch_counts = [count_leaves(b) for b in branches(tree)]
+        return sum(branch_counts)
+def partition_tree(n, m):
+    """return a partition tree of n using parts of up to m"""
+    if n == 0:
+        return tree(True)
+    elif n < 0 or m == 0:
+        return tree(False)
+    else:
+        left = partition_tree(n-m, m)
+        right = partition_tree(n, m-1)
+        return tree(m, [left, right])
+
+def print_parts(tree, partition=[]):
+    if is_leaf(tree):
+        if label(tree):
+            print(' + '.join(partition))
+        else:
+            left, right = branches(tree)
+            m = str(label(tree))
+            print_parts(left, partition + [m])
+            print_parts(right, partition)
