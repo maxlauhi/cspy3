e8eb0aa27d6921d66c8e28a6e43cd016f28b7eae
maxlauhi <maxlauhi@gmail.com>
2018-07-17 18:02:37 +0800

    update bank system using subclass

--- 2_5_2_classes.py
+++ 2_5_2_classes.py
@@ -1,15 +1,22 @@
 class Account:
+    """A bank account that has a non-negative balance."""
+    interest = 0.02
     def __init__(self, account_holder):
         self.balance = 0
         self.holder = account_holder
     def deposit(self, amount):
+        """Increase the account balance by amount and return the new balance."""
         self.balance = self.balance + amount
         return self.balance
     def withdraw(self, amount):
+        """Decrease the account balance by amount and return the new balance."""
         if amount > self.balance:
             return 'Insufficient funds'
         self.balance = self.balance - amount
         return self.balance
-
-
-
+class CheckingAccount(Account):
+    """A bank account chage of withdrawals"""
+    withdraw_chage = 1
+    interest = 0.01
+    def withdraw(self, amount):
+        return Account.withdraw(self,amount + self.withdraw_chage)
