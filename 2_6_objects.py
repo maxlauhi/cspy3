5c464ff317422fc9a8e2acd1876f09102d6d7d74
maxlauhi <maxlauhi@gmail.com>
2018-07-23 17:49:38 +0800

    Implementing classes and objects.

---
+++ 2_6_objects.py
@@ -0,0 +1,13 @@
+def make_instance(cls):
+    """Return a new object, which is a dispatch dictionary."""
+    def get_value(name):
+        if name in attributes:
+            return attributes[name]
+        else:
+            value = cls['get'](name)
+            return bind_method(value, instance)
+    def set_value(name, value):
+        attributes[name] = value
+    attributes = {}
+    instance = {'get': get_value, 'set': set_value}
+    return instance
